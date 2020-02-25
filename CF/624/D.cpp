c9e595200464eebc00e83a1583f086e955402b03
Abhi <abhish014@rediffmail.com>
2020-02-26 01:01:21 +0530

    added 4 sub and edited a mistake

--- CF/624/D.cpp
+++ CF/624/D.cpp
@@ -7,9 +7,9 @@ int main(int argc, char const *argv[])
 {
 	
 
-	vector<int> div[10004];
-	// vector<vector<int> > div2(10004, vector<int> ());
-	for(int i = 1; i < 10003; ++i) {
+	vector<int> div[20014];
+	// vector<vector<int> > div2(30004, vector<int> ());
+	for(int i = 1; i < 20014; ++i) {
 		set<int>S;
 		for(int j = 1; j*j<=i; ++j) {
 			if(i%j==0)S.insert(j);
@@ -25,7 +25,7 @@ int main(int argc, char const *argv[])
 		cin >> a >> b >> c;
 		int fo=-1,fi = -1, fa=-1, mn = INT_MAX;
 
-		for(int x = 1; x <= 1e4; ++x) {
+		for(int x = 1; x <= 2e4+10; ++x) {
 			int n1 = (c/x)*x, n2 = (c/x+1)*x;
 			int d3 = min(abs(n1-c),abs(n2-c));
 			int fotmp;
