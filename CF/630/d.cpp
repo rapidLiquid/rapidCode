5985ffc45372d4465ba592239cb16364881f9bf6
Abhi <abhish014@rediffmail.com>
2020-04-01 01:44:14 +0530

    630 - b remaining

--- CF/630/d.cpp
+++ CF/630/d.cpp
@@ -1,4 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <stack>
+#include <queue>
+#include <set>
+#include <algorithm>
+
 using namespace std;
  
 #define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
@@ -27,63 +34,26 @@ const int inf = 2e9;
 const double eps = 1e-10;
  // START FROM HERE
 
-
-vi _si(int n) {
- vi pr(n+1,1);
- pr[0] = pr[1] = 0;
- fr(i,2,n+1) {
-  if (!pr[i]) continue;
-  for(int j = 2*i; j < n+1; j+=i)
-   pr[j] = 0;
- }
- vi res;
- fr(i,0,n+1) if(pr[i]) res.pb(i);
- return res;
-}
-
-
 int main(int argc, char const *argv[])
 {
- fastio;
- 
- t_times {
-  int n;cin >> n;
-  vi a(n);
-  fr(i,0,n)cin>>a[i];
-
-  vi ans(n,0);
-  
-  int color=0;
-  vi till[13];
-  sort(all(a));
-  till[color].pb(0);
-
-
-  fr(i,1,n) {
-  	int flag2=0;
-  	fr(j,0,color+1) {
-  		int flag = 1;
-  		fr(k,0,(int)till[j].size()) {
-  			if(__gcd(a[till[j][k]],a[i])==1){
-  				flag=0;break;
-  			}
-  		}
-  		if(flag){till[j].pb(i);flag2=1;break;}
-  	}
-  	if(!flag2){
-  		++color;
-  		till[color].pb(i);
-  	}
+	fastio;
+	
+	int k;
+	cin >> k;
+	if(k == 0) {
+		cout << "1 1\n" << 1 << endl;
+	} else {
+		// b > k and a is fully set
+		int a = 1<<18;
+		a-=1;
+		int b = 1<<17;
+		//a is all set
+		// 
+		cout << "2 3\n";
+		cout << a << " " << k << " " << 0 << endl;
+		cout << b << " " << a << " " << k << endl;
+	}
 
 
-  }
-  fr(c,0,color+1){
-  	for(int i : till[c]) ans[i]=c+1;
-  }
-  cout << color+1 << endl;
-  for(int i : ans)cout << i << " ";
-  	cout << endl;
-
- }
- return 0;
+	return 0;
 }
