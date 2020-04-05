c72920898b9e4ae71cbb6c61926d10f6b26d0c50
Abhi <abhish014@rediffmail.com>
2020-04-06 01:59:02 +0530

    some haskell , some cp

---
+++ CF/631/a.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <stack>
+#include <queue>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+ 
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define t_times int t; cin >> t; while(t--)
+#define _parr(a, n) for(int __i = 0; __i < (int )n; __i++) cout << a[__i] << ' '; cout << '\n'
+#define fr(i, st, n) for(int i = (int )st; i < (int )n; i++)
+#define frn(i, st, n) for(int i = (int )st; i <= (int )n; i++)
+#define rev(i, en, st) for(int i = (int )en; i >= (int )st; i--)
+#define all(c) (c).begin(), (c).end()
+#define rall(c) (c).begin(), (c).end()
+#define mp make_pair
+#define F first
+#define S second
+#define pb push_back
+ 
+typedef long long int LL;
+typedef unsigned long long ULL;
+typedef unsigned int uint;
+typedef pair< int, int> ii;
+typedef vector< int> vi;
+typedef vector< vi> vvi;
+ 
+const int mod = 1e9 + 7;
+const LL INF = 9e18 + 2e17;
+const int inf = 2e9;
+const double eps = 1e-10;
+ // START FROM HERE
+
+int main(int argc, char const *argv[])
+{
+	fastio;
+	t_times {
+		int n, x;cin >> n>> x;
+		set<int>S;
+		fr(i,0,n){
+			int y;cin>>y;
+			S.insert(y);
+		}
+		int ans = 1;
+
+		fr(i,1,300) {
+			// is it poss to go till ith
+			set<int>SS(all(S));
+			int miss=0;
+			fr(j,1,i+1) {
+				if(SS.find(j)==SS.end()){
+					miss++;
+					SS.insert(j);
+				}
+			}
+			if(miss<=x)ans=i;
+		}
+
+		cout << ans<<endl;
+	}
+
+
+
+	return 0;
+}
